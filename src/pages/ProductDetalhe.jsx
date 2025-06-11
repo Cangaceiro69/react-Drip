0183e9ac8022e6a4e12aa4e2c09dd34232693b06
Matheus Fontenele <gokuback100@gmail.com>
2025-06-11 15:48:08 -0300

    carrinho

--- src/pages/ProductDetalhe.jsx
+++
@@ -1,9 +0,0 @@
-import ProductDisplay from "./pages/productDisplay";
-
-export default function ProductDetalhe() {
-  return (
-    <div className="min-h-screen bg-gray-100 flex items-center justify-center p-8">
-      <ProductDisplay />
-    </div>
-  );
-}
